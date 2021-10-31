178968bfe73e591dadd9b7ea7d3b261d42ff8463
Ashik-Himel <ashikujjamanhimel@gmail.com>
2021-10-31 19:51:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
-const navLink = document.querySelectorAll('.navigation__link')
+const navLink = document.querySelectorAll('.navigation__link');
 
 hamburger.onclick = function() {
     navigation.classList.toggle('show-nav');
