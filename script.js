a8dcc5f59480adc363386cf1bb007ab770db1d00
Ashik-Himel <ashikujjamanhimel@gmail.com>
2021-11-01 09:32:37 +0600

    window click close added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
 const navLink = document.querySelectorAll('.navigation__link');
+const navSection = document.querySelector('.navigation-section')
+
+function navAndCrossClose() {
+    navigation.classList.remove('show-nav');
+    hamburger.classList.remove('cross-hamburger');
+}
 
 hamburger.onclick = function() {
     navigation.classList.toggle('show-nav');
@@ -9,6 +15,14 @@ hamburger.onclick = function() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 function linkAction(){
-    navigation.classList.remove('show-nav');
-    hamburger.classList.remove('cross-hamburger');
+    return navAndCrossClose();
 }
+
+window.addEventListener('click' , function(e) {
+    if(navSection.contains(e.target)) {
+
+    }
+    else{
+        return navAndCrossClose();
+    }
+});
