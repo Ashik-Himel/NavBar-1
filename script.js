e155a2397d7f48e5caab70266a9acfe50c4a6046
Ashik-Himel <ashikujjamanhimel@gmail.com>
2021-11-04 14:43:59 +0600

    minimized code

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
-const navLink = document.querySelectorAll('.navigation__link');
-const navSection = document.querySelector('.navigation-section');
+const navLink = document.querySelectorAll('.nav-link');
+const navSection = document.querySelector('.nav-section');
 
+function navAndCrossToggle() {
+    navigation.classList.toggle('show-nav');
+    hamburger.classList.toggle('cross-hamburger');
+}
 function navAndCrossClose() {
     navigation.classList.remove('show-nav');
     hamburger.classList.remove('cross-hamburger');
 }
 
-hamburger.onclick = function() {
-    navigation.classList.toggle('show-nav');
-    hamburger.classList.toggle('cross-hamburger');
-}
-
-navLink.forEach(n => n.addEventListener('click', linkAction));
-function linkAction(){
-    return navAndCrossClose();
-}
+hamburger.onclick = navAndCrossToggle;
+navLink.forEach(n => n.addEventListener('click', navAndCrossClose));
 
 window.addEventListener('click' , function(e) {
     if(navSection.contains(e.target)) {
-
+        
     }
     else{
         return navAndCrossClose();
